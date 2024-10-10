57bacab5cd1d89879a0c64009aa31505abdcc303
Vladick <vlad2004ki@gmail.com>
2024-10-10 22:29:10 +0300

    pySide меня убил, его не будет

--- Lab-3/main.py
+++ Lab-3/main.py
@@ -1,16 +1,1 @@
-import  sys
-from PySide6.QtWidgets import QApplication, QMainWindow
-from ui_main import Ui_MainWindow
-
-class Application(QMainWindow):
-    def __init__(self):
-        super(Application, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = Application()
-    window.show()
-
-    sys.exit(app.exec())
+import flet as ft
--- Lab-3/server/database_connection.py
+++ Lab-3/server/database_connection.py
@@ -1,5 +1,7 @@
 import psycopg2
 from psycopg2 import Error
+from advertisements import Advertisements
+
 
 def database_connect():
     try:
@@ -10,22 +12,46 @@ def database_connect():
             user='postgres',
             password='admin'
         )
+        connection.autocommit = False
 
         return connection
     except (Exception, Error) as error:
         print("Connection ERROR", error)
         return None
 
+
 def make_query(connection, query):
     try:
         cursor = connection.cursor()
         cursor.execute(query)
-        return cursor
+        result = cursor.fetchall()
+        return result
     except (Exception, Error) as error:
         print("Query ERROR", error)
         return None
+
 
 def database_close_connection(connection):
     if connection is not None:
         connection.close()
         print("Connection Closed")
+
+
+def insert_data(data: Advertisements, connection):
+    try:
+        id = str(make_query(connection, "SELECT COUNT(*) FROM products;"))
+        cursor = connection.cursor()
+        cursor.execute(
+            f"INSERT INTO products (id, title, description, price, category, images, seller_contacts) VALUES ({id}, "
+            f"{data.title}, {data.description}, {data.price}, {data.category}, {data.images}, {data.seller_contacts});")
+        rowcount = cursor.rowcount
+        if rowcount == 1:
+            print("Insert successfully.")
+        else:
+            print("Insert failed.")
+
+        connection.commit()
+        return cursor
+    except (Exception, Error) as error:
+        print("Query ERROR", error)
+        return None
